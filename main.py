d9f9120e35db89c2268812c1765dcd52143c9d82
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-04-26 19:18:32 -0500

    dt variable made to calc the time that passed since last frame was drawn

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -13,6 +14,7 @@ def main():
                 return
         screen.fill("Black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")

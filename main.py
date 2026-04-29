e2ade42a9bb73c213c76126b841991dfa4d2ce6f
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-04-28 22:22:19 -0500

    added collison detection of ship and asteroids, prints game over and logs the collison

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import sys
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from circleshape import CircleShape
 
 def main():
     pygame.init()
@@ -12,12 +16,21 @@ def main():
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    AstroField = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game Over")
+                sys.exit()
         screen.fill("Black")
         for drawables in drawable:
             drawables.draw(screen)

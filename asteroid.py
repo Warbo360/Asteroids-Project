869f4d575d10fe8ac642aacf484772d430c56d83
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-04-29 22:28:35 -0500

    added .split() method to Asteroid class when a shot collides with an asteroid

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
+import random
 import pygame
-from constants import LINE_WIDTH
+from constants import *
 from circleshape import CircleShape
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        log_event("asteroid_split")
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            rand_angle = random.uniform(20, 50)
+            new_asteroid1_vector = self.velocity.rotate(rand_angle)
+            new_asteroid2_vector = self.velocity.rotate(-rand_angle)
+            new_asteroid1 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+            new_asteroid2 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+            new_asteroid1.velocity = new_asteroid1_vector * 1.2
+            new_asteroid2.velocity = new_asteroid2_vector * 1.2
--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
         screen.fill("Black")
         for drawables in drawable:
             drawables.draw(screen)
